2833be6144ea437d6290140fda31da4e565bee77
Ferdinand van Wyk <ferdinandvwyk@gmail.com>
2016-03-08 16:02:17 +0000

    add sparse tensor test

--- test/test_graphkit.rb
+++ test/test_graphkit.rb
@@ -6,7 +6,17 @@ class TestGraphkit < Minitest::Unit::TestCase
     FileUtils.makedirs('test/test_output')
   end
 
-  def test_basic
+  def teardown
+    FileUtils.rm_r('test/test_output')
+  end
+
+  def test_sparse_tensor_new
+    t = SparseTensor.new(3)
+    assert_equal(t.shape, [0,0,0])
+    assert_equal(t.rank, 3)
+  end
+
+  def test_multiplot
     a = GraphKit.autocreate({x: {data: [2, 5, 11, 22],
                                  units: 'years',
                                  title: 'Age'},
@@ -28,16 +38,6 @@ class TestGraphkit < Minitest::Unit::TestCase
     b.close
     b.gnuplot_write('test/test_output/heights.ps')
 
-    c = SparseTensor.new(3)
-    c[1,3,9]= 4
-    c[3,3,34] = 4.346
-    c[23, 234, 293] = 9.234
-
-    d = SparseTensor.new(3)
-    d[1,3,9]= 4
-    d[3,3,34] = 4.346
-    d[23, 234, 294] = 9.234
-
     multiplot = GraphKit::MultiWindow.new
     multiplot.push a
     multiplot.push b
@@ -50,7 +50,4 @@ class TestGraphkit < Minitest::Unit::TestCase
     multiplot.gnuplot_write('test/test_output/multiplot.ps', multiplot: 'layout 2,2')
   end
 
-  def teardown
-    FileUtils.rm_r('test/test_output')
-  end
 end
